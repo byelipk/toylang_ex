055d7dc05a98c1468d3444c5c7cb999547ea15fb
Pat White <byelipk@gmail.com>
2024-05-02 14:24:41 -0400

    Adding string support to lexer. Tests failing

--- lib/toylang/core/lexer.ex
+++ lib/toylang/core/lexer.ex
@@ -11,34 +11,54 @@ defmodule Toylang.Core.Lexer do
   #
   # var ten = 10
   def get_tokens(input) do
-    tokenize(input |> String.graphemes, "", [])
+    tokenize(input |> String.trim |> String.graphemes, "", [])
   end
 
-  defp tokenize([], "", tokens) do
-    tokens ++ finalize_token("")
-  end
-
+  defp tokenize([], "", []), do: finalize_token("")
+  defp tokenize([], "", tokens), do: tokens
   defp tokenize([], current_token, tokens) do
-    tokens ++ finalize_token(current_token) ++ finalize_token("")
+    final_token = finalize_token(current_token)
+    case final_token do
+      [{:eof}] -> tokens ++ final_token
+      _ -> tokens ++ final_token ++ finalize_token("")
+    end
   end
-
   defp tokenize([char | rest], current_token, tokens) do
     case char do
-      "\s" when current_token != "" ->
-        new_tokens = tokens ++ finalize_token(current_token)
-        tokenize(rest, "", new_tokens)
+      # Null bytes
+      "\0" ->
+        tokenize(rest, "", tokens ++ finalize_token(""))
 
       "\s" ->
-        tokenize(rest, current_token, tokens)
+        cond do
+          has_opening_quote?(current_token) -> 
+            tokenize(rest, current_token <> char, tokens)
+          current_token != "" -> 
+            new_tokens = tokens ++ finalize_token(current_token)
+            tokenize(rest, "", new_tokens)
+          true -> 
+            tokenize(rest, current_token <> char, tokens) 
+        end
 
+      # Newline
       "\n" ->
         new_tokens = tokens ++ finalize_token(current_token)
         tokenize(rest, "", new_tokens)
 
+      # Tab
       "\t" ->
         new_tokens = tokens ++ finalize_token(current_token)
         tokenize(rest, "", new_tokens)
 
+      # Opening quote for a string
+      "\"" when current_token == "" -> 
+        tokenize(rest, current_token <> char, tokens)
+
+      # Closing quote for a string
+      "\"" when current_token != "" -> 
+        tokenize(rest, current_token <> char, tokens)
+
+      # Other characters
       _ ->
         cond do
           is_letter?(char) -> 
@@ -60,18 +80,30 @@ defmodule Toylang.Core.Lexer do
     token =~ ~r/[0-9]/
   end
 
-  defp finalize_token(""), do: [{:eof, "eof"}]
+  defp has_opening_quote?(token) do
+    String.first(token) == "\""
+  end
+
+  defp has_closing_quote?(token) do
+    String.last(token) == "\""
+  end
+
+  defp is_string?(token) do
+    has_opening_quote?(token) && has_closing_quote?(token)
+  end
+
+  defp finalize_token(""), do: [{:eof}]
   defp finalize_token(token) do
     case token do
-      "def" -> [{:method_def, token }]
-      "do" -> [{:method_open, token }]
-      "end" -> [{:method_close, token }]
-      "return" -> [{:method_ret, token}]
+      "def" -> [{:define, nil}]
+      "do" -> [{:do, nil}]
+      "end" -> [{:end, nil}]
+      "return" -> [{:return, nil }]
       _ -> 
-        if is_digit?(token) do
-          [{:number, token}]
-        else
-          [{:identifier, token}]
+        cond do
+          is_digit?(token) -> [{:integer, token}]
+          is_string?(token)  -> [{:string, token}]
+          true -> [{:identifier, token}]
         end
     end
   end
--- test/toylang_core_lexer_test.exs
+++ test/toylang_core_lexer_test.exs
@@ -2,21 +2,54 @@ defmodule Toylang.Core.LexerTest do
   use ExUnit.Case
   alias Toylang.Core.Lexer
 
-  test "parse empty string" do
-    assert Lexer.get_tokens("") == [{:eof, "eof"}]
-  end
-
-  test "parse hello world" do
-    source = "def hello do\n return 10\nend"
+  @tag :pending
+  test "it parses a method definition" do
+    lines = [
+      "def hello do\n",
+      " return 10\n",
+      "end\n",
+      "\n",
+      "def world do\n",
+      " return \"Hello, world!\"\n",
+      "end\n",
+      "\n",
+    ]
+    source = List.to_string(lines)
     tokens = Lexer.get_tokens(source)
     assert tokens == [
-      {:method_def, "def"},
+      {:define, nil},
       {:identifier, "hello"},
-      {:method_open, "do"},
-      {:method_ret, "return"},
-      {:number, "10"},
-      {:method_close, "end"},
-      {:eof, "eof"}
+      {:do, nil},
+      {:return, nil},
+      {:integer, "10"},
+      {:end, nil},
+      {:define, nil},
+      {:identifier, "world"},
+      {:do, nil},
+      {:return, nil},
+      {:string, "\"Hello, world!\""},
+      {:end, nil},
+      {:eof}
     ]
+  end
+
+  test "parse empty string" do
+    assert Lexer.get_tokens("") == [{:eof}]
+  end
+
+  test "parse number" do
+    assert Lexer.get_tokens("12345") == [{:integer, "12345"}, {:eof}]
+  end
+
+  test "it handles null bytes" do
+    assert Lexer.get_tokens("\0") == [{:eof}]
+  end
+
+  test "it handles whitespace" do
+    assert Lexer.get_tokens(" ") == [{:eof}]
+  end
+
+  test "it handles strings" do
+    assert Lexer.get_tokens("\"hello\"") == [{:string, "\"hello\""}, {:eof}]
   end
 end
